e6f1072ed38388b36d0ea6c0c394809a336bc327
BT_1N3_26 <sample@bths.edu>
2024-10-25 13:18:03 -0400

    print statements and change the variable name.

--- src/AvashRunner.java
+++ src/AvashRunner.java
@@ -1,7 +1,8 @@
 public class AvashRunner {
     public static void main(String[] args) {
         System.out.println("hello world.");
-        int x = 10;
+        int y = 10;
         System.out.println("a change is the creation of x and the setting to 10.");
+        System.out.println(y + " this is also a change as i changed the x variable to y");
     }
 }
