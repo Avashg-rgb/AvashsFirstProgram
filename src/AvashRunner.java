ac36b398ecedebd082781c9defd92229c7499819
BT_1N3_26 <sample@bths.edu>
2024-10-25 13:14:47 -0400

    added some logic.

--- src/AvashRunner.java
+++ src/AvashRunner.java
@@ -1,5 +1,7 @@
 public class AvashRunner {
     public static void main(String[] args) {
         System.out.println("hello world.");
+        int x = 10;
+        System.out.println("a change is the creation of x and the setting to 10.");
     }
 }
